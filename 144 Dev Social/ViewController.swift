9dad1de3762e49ce4c41af9bd3d28d07a24d2cc4
chaloemphol <chaloemphol@hotmail.com>
2016-10-13 13:53:44 +0700

    Initial Commit

---
+++ 144 Dev Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  144 Dev Social
+//
+//  Created by Chaloemphol Preedarat on 13/10/2016.
+//  Copyright © 2016 Chaloemphol Preedarat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
